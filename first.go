7dcd08479aa9490727d7b697f82d630f0e80eef5
shekar22368 <158766047+shekar22368@users.noreply.github.com>
2024-02-03 21:06:30 +0330

    Create first.go

---
+++ first.go
@@ -0,0 +1,9 @@
+$ mkdir hello # Alternatively, clone it if it already exists in version control.
+$ cd hello
+$ go mod init example/user/hello
+go: creating new go.mod: module example/user/hello
+$ cat go.mod
+module example/user/hello
+
+go 1.16
+$
